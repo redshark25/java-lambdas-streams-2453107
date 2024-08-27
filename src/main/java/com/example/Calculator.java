a294181def9d777ec8cb173c65f87926809c5147
redshark25 <tnsenurafernando@gmail.com>
2024-08-27 21:06:53 +0200

    Lambda implementation

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,7 @@
+package com.example;
+
+@FunctionalInterface
+public interface Calculator {
+
+    int calculate(int x, int y);
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import java.util.Random;
+import java.util.function.Function;
+import java.util.function.IntBinaryOperator;
+
 public class Main {
 
   public static void main(String[] args) {
@@ -15,6 +19,32 @@ public class Main {
     };
     greeting2.sayHello();
 
+    Greeting greeting3 = ()-> System.out.println("Hello World using lambda Fuction");
+    greeting3.sayHello();
+
+    Calculator calculator = (int x, int y)-> {
+      Random random = new Random();
+      int randomNumber = random.nextInt(100);
+      return randomNumber+x+y;
+    };
+    System.out.println(calculator.calculate(1,2));
+
+    IntBinaryOperator calculator2 = (int x, int y) ->{
+      return x+y;
+
+    };
+
+   Function <String, Integer> function = (str) -> str.length();
+    System.out.println("using lambda "+function.apply("hello world"));
+
+    System.out.println("test "+calculator2.applyAsInt(1,8));
   }
+
+
+
+
+
+
+
 
 }
